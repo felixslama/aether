4acee8183ce1af280b53d55b7e488bbecd87d94f
fazlicm <fazlicm@edu.htl-villach.at>
2021-10-23 15:12:29 +0200

    Wow a Template

---
+++ software/testcode/ESP32_Rocket_Gyro_Servo_withweb/WebInterface/main.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.toggle input')
+
+toggle.addEventListener('click', () => {
+    const onOff = toggle.parentNode.querySelector('.onoff')
+    onOff.textContent = toggle.checked ? 'ON' : 'OFF'
+})
