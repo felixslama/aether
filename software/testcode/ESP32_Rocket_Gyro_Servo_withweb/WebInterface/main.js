d3694c21f29e6d93262389b363cb70cbceaa1eda
Mercy <fazlicm@edu.htl-villach.at>
2021-11-04 16:39:31 +0100

    Webinterface

--- software/testcode/ESP32_Rocket_Gyro_Servo_withweb/WebInterface/main.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.toggle input')
-
-toggle.addEventListener('click', () => {
-    const onOff = toggle.parentNode.querySelector('.onoff')
-    onOff.textContent = toggle.checked ? 'ON' : 'OFF'
-})
