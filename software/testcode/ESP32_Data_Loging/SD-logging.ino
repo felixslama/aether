205b3ac95721695ec2bc740628b28db7821c0b25
B333F <92101877+B333F@users.noreply.github.com>
2021-11-22 19:12:33 +0100

    update SD-Logging

--- software/testcode/ESP32_Data_Loging/SD-logging.ino
+++ software/testcode/ESP32_Data_Loging/SD-logging.ino
@@ -1,4 +1,5 @@
 /*
+ * ESP32-WROOM
  * Connect the SD card to the following pins:
  *
  * SD Card | ESP32
@@ -12,102 +13,59 @@
  *    D0       MISO
  *    D1       -
  */
+
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
 #include <string>
 
-
+//pinout SD|ESP32
 #define SCK  18
 #define MISO  19
 #define MOSI  23
 #define CS  5
+//VCC       5V
+//GND       Ground
 
 
-void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("File written");
-    } else {
-        Serial.println("Write failed");
-    }
-    file.close();
+//void setup(){
+void initLog(){
+    Serial.begin(115200); 
 }
 
-void appendFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Appending to file: %s\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if(!file){
-        Serial.println("Failed to open file for appending");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("Message appended");
-    } else {
-        Serial.println("Append failed");
-    }
-    file.close();
-}
-
-
-
-void setup(){
-    Serial.begin(115200);
-    if(!SD.begin()){
-        Serial.println("Card Mount Failed");
-        return;
-    }
-    uint8_t cardType = SD.cardType();
-
-    if(cardType == CARD_NONE){
-        Serial.println("No SD card attached");
-        return;
-    }
-
-    Serial.print("SD Card Type: ");
-    if(cardType == CARD_MMC){
-        Serial.println("MMC");
-    } else if(cardType == CARD_SD){
-        Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
-
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-    Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-/*    
-    writeFile(SD, "/hello.txt", "Hello ");
-    appendFile(SD, "/hello.txt", "World!\n");
-    testFileIO(SD, "/test.txt");
-    */
-    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
-}
 
 void loop(){
-    writeFile(SD, "/log.txt", "Log");
-    delay(10);
-    appendFile(SD, "/log.txt", "log entry");
+    String a = "1";
+    string_to_sdcard(a);
+    String b = "2";
+    string_to_sdcard(b);
+    String c = "3";
+    string_to_sdcard(c);
+    delay(1000);
 
-    for(int i = 0; i < 999999; i++){
-        char str[8];
-        itoa( i, str, 10 );
-        appendFile(SD, "/log.txt", "Entry: ");
-        appendFile(SD, "/log.txt", str);
-        appendFile(SD, "/log.txt", "\n");
+}
 
-
-        Serial.print("SD-Entry: ");
-        Serial.print(str);
+void writeLog(String logData){
+    //the beginning of log.txt file is buggy, even with logData from previos test runs.
+    const char path[] = "/log.txt";
+    const char *logChar = logData.c_str();
+    
+    
+    if(!SD.begin(CS)) {
+      Serial.println("Card Mount Failed");
+      return;
+    } 
+    fs::FS &fs = SD;
+    //Serial.printf("Appending to file: %s\n", path);
+    File file = fs.open(path, FILE_APPEND);
+    
+    if(!file){
+        Serial.println("Failed to open file");
+        return;
     }
+    if(!file.println(logChar)){
+        Serial.println("Failed writing to file");
+        return;
+    } 
+    //file.close();
 }
