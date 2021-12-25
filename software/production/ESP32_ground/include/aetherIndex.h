3215914a14a2bccb1a29e82a7530b816ddf71fee
K1llf0rce <fteppan@protonmail.com>
2021-12-25 19:55:51 +0100

    add OTA to groundstation

--- software/production/ESP32_ground/include/aetherIndex.h
+++ software/production/ESP32_ground/include/aetherIndex.h
@@ -113,3 +113,125 @@ const char aetherIndex[] PROGMEM = R"=====(
 
 </html>
 )=====";
+const char updateIndex[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            * {
+                background-color: black;
+                font-family: Arial, Helvetica, sans-serif
+            }
+            p, h1, label {
+                color: white;
+                font-size: 40px;
+            }
+            a {
+                text-decoration: none;
+                color: white;
+            }
+            button {
+                background-color: black;
+                color: white;
+                border: solid white;
+                margin: 10px;
+                font-size: 40px;
+                border-radius: 15px;
+            }
+            input {
+                color: white;
+                margin: 10px;
+                font-size: 20px;
+                border-radius: 15px;
+            }
+            #mainContainer {
+                border: solid white;
+                border-width: 10px;
+                text-align: center;
+                margin: 0 auto;
+                padding: 10px;
+            }
+            #submitButton {
+                background-color: black;
+                color: white;
+                border: solid white;
+                margin: 10px;
+                font-size: 20px;
+                border-radius: 15px;
+            }
+            @media (pointer:none), (pointer:coarse) {
+                p, h1, label {
+                    font-size: 70px;
+                }
+                button {
+                    font-size: 70px;
+                }
+                input {
+                    font-size: 30px;
+                }
+                #submitButton {
+                    font-size: 40px;
+                }
+            }
+        </style>
+        <script>
+            function notify_update() {
+                document.getElementById("update").innerHTML = "<p>Updating...</p>";
+            }
+        </script>
+    </head>
+    <body>
+        <section id="mainContainer">
+            <h1>AETHER-OTA</h1>
+            <p>Only *.bin files will work!</p>
+            <form method='POST' action='/doUpdate' enctype='multipart/form-data' target='_self' onsubmit='notify_update()'>
+                <input type='file' name='update' accept=".bin" required><br>
+                <input id="submitButton" type='submit' value='Start Update!'>
+            </form>
+            <div id="update"></div>
+        </section>
+    </body>
+</html>
+)=====";
+const char updateDoneIndex[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            * {
+                background-color: black;
+                font-family: Arial, Helvetica, sans-serif
+            }
+            p, h1, label {
+                color: white;
+                font-size: 40px;
+            }
+            a {
+                text-decoration: none;
+                color: white;
+            }
+            @media (pointer:none), (pointer:coarse) {
+                p, h1, label {
+                    font-size: 70px;
+                }
+            }
+        </style>
+        <script>
+            let counter = 4;
+            setTimeout(refresh, 5000);
+            var upInt = setInterval(updateNumber, 1000);
+            function refresh() {
+                clearInterval(upInt);
+                window.location.replace("/");
+            }
+            function updateNumber() {
+                document.getElementById("updateText").innerHTML = "Update done! Refreshing in " + counter + " ..."
+                counter--;
+            }
+        </script>
+    </head>
+    <body>
+        <p id="updateText">Update done! Refreshing in 5 ...</p>
+    </body>
+</html>
+)=====";
--- software/production/ESP32_ground/src/aetherWebinterface.cpp
+++ software/production/ESP32_ground/src/aetherWebinterface.cpp
@@ -4,6 +4,7 @@
 #include "aetherIndex.h"
 #include "aetherLora.h"
 #include <ESPAsyncWebServer.h>
+#include <Update.h>
 
 float calcValue;
 
@@ -16,12 +17,52 @@ void inputGyro(float newValue){
     calcValue = newValue;
 }
 
+void handleUpdate(AsyncWebServerRequest *request) {
+  request->send(200, "text/html", updateIndex);
+};
+
+void handleDoUpdate(AsyncWebServerRequest *request, const String& filename, size_t index, uint8_t *data, size_t len, bool final) {
+  if (!index){
+    size_t content_len;  
+    content_len = request->contentLength();
+    int cmd = (filename.indexOf(F(".spiffs.bin")) > -1 ) ? U_SPIFFS : U_FLASH;
+    if (cmd == U_FLASH && !(filename.indexOf(F(".bin")) > -1) ) return; // wrong image for ESP32
+    if (!Update.begin(UPDATE_SIZE_UNKNOWN, cmd)) {
+      Update.printError(Serial);
+    }
+  }
+  if (Update.write(data, len) == len) {
+    int progress = Update.progress() / 10000;
+    Serial.println(progress);
+  } else {
+    Update.printError(Serial);
+  }
+  if (final) {    
+    if (!Update.end(true)){
+      Update.printError(Serial);
+    } else {
+      AsyncWebServerResponse *response = request->beginResponse(200, "text/html", updateDoneIndex); 
+      request->send(response); 
+      delay(100);
+      ESP.restart();
+    }
+  }
+}
+
 void initWeb(){
     WiFi.mode(WIFI_AP);
     WiFi.softAP(ssid, password);
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
         request->send_P(200, "text/html", aetherIndex);
     });
+    server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/html", updateIndex);
+    });
+    server.on("/doUpdate", HTTP_POST,
+        [](AsyncWebServerRequest *request) {},
+        [](AsyncWebServerRequest *request, const String& filename, size_t index, uint8_t *data, size_t len, bool final) {
+        handleDoUpdate(request, filename, index, data, len, final);}
+    );
     server.on("/readLog", HTTP_GET, [] (AsyncWebServerRequest *request) {
         String logvalue = readLora();
         String logData = logData + logvalue;
